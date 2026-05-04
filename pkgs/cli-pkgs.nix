6c9757836b4d66d4bb21db7ceb74611ae5a21d60
Nicolas M <marshall.nicolas@gmail.com>
2026-05-04 10:38:19 +0100

    cli: add rmrfrs

--- pkgs/cli-pkgs.nix
+++ pkgs/cli-pkgs.nix
@@ -435,4 +435,37 @@
   #     });
   # };
 
+
+  pkgDefs.rmrfrs = rec {
+    versions.aarch64-darwin."0.8.8" = { sha256 = "1QF1l6V6YmKDPqlbXpMeWg3Pt5AonBHelD63mJkSWNM="; cargoHash = "sha256-mPQN/JN6b9/1xo1JSj0LpVjb7rGTwrfU0PY7pbENdg4="; };
+    mkPkg = { pkgs, version ? (l.latest versions.${system}), system ? pkgs.stdenv.hostPlatform.system, ... }:
+      let vData = versions.${system}.${version};
+      in pkgs.rustPlatform.buildRustPackage (finalAttrs: {
+        pname = "rmrfrs";
+        inherit version;
+
+        src = pkgs.fetchFromGitHub {
+          owner = "trinhminhtriet";
+          repo = "rmrfrs";
+          tag = "v${finalAttrs.version}";
+          sha256 = vData.sha256;
+        };
+
+        cargoHash = vData.cargoHash;
+
+        passthru.updateScript = nix-update-script { };
+
+        meta = {
+          description = "Powerful filesystem cleaning tool designed to optimize storage by identifying and removing unnecessary files within known project structures";
+          homepage = "https://github.com/trinhminhtriet/rmrfrs";
+          downloadPage = "https://github.com/trinhminhtriet/rmrfrs";
+          changelog = "https://github.com/trinhminhtriet/rmrfrs/blob/v${finalAttrs.version}/CHANGELOG.md";
+          license = l.licenses.mit;
+          maintainers = with l.maintainers; [ adda ];
+          mainProgram = "rmrfrs";
+          platforms = with l.platforms; windows ++ darwin ++ linux;
+        };
+      });
+  };
+
 }
